81e33191b35863efa3882dc6a19ea902246254e8
przemek <przlada@gmail.com>
2015-01-05 23:26:41 +0100

    Automatyczne generowanie JAR do testow

--- src/pl/edu/pw/elka/tin/MNC/Main.java
+++ src/pl/edu/pw/elka/tin/MNC/Main.java
@@ -1,10 +1,9 @@
 package pl.edu.pw.elka.tin.MNC;
 
-import pl.edu.pw.elka.tin.MNC.MNCConstants.MNCConsts;
 import pl.edu.pw.elka.tin.MNC.MNCConstants.MNCDict;
 import pl.edu.pw.elka.tin.MNC.MNCController.MNCController;
+import pl.edu.pw.elka.tin.MNC.MNCController.MNCDevice;
 import pl.edu.pw.elka.tin.MNC.MNCController.MNCMonitor;
-import pl.edu.pw.elka.tin.MNC.MNCNetworkProtocol.MNCDatagram;
 
 
 
@@ -13,28 +12,16 @@ public class Main {
     public static void main(String[] args) {
         MNCDict.Langs lang = MNCDict.Langs.PL;
         try {
-            MNCController controller1 = new MNCController("controller1",new MNCAddress("1", MNCAddress.TYPE.CONTROLLER),new MNCSystemLog(lang,"controller1"));
-            MNCController controller2 = new MNCController("controller2",new MNCAddress("2", MNCAddress.TYPE.CONTROLLER),new MNCSystemLog(lang,"controller2"));
-            controller1.addGroup("grupa");
-            controller2.addGroup("grupa");
-            //controller.addToken("grupa");
-
-            MNCMonitor monitor1 = new MNCMonitor("monitor1", new MNCAddress("3", MNCAddress.TYPE.MONITOR),new MNCSystemLog(lang,"monitor1"));
-          //  MNCMonitor monitor2 = new MNCMonitor("monitor2", new MNCAddress("3", MNCAddress.TYPE.MONITOR),new MNCSystemLog(lang,"monitor2"));
-
-            monitor1.addGroup("grupa");
-           // monitor2.addGroup("grupa");
-
-            //monitor1.sendDatagram(new MNCDatagram(monitor1.getMyAddress(), MNCConsts.MULTICAST_ADDR, "grupa", MNCDatagram.TYPE.IAM_IN_GROUP, null));
-            //monitor2.sendDatagram(new MNCDatagram(monitor2.getMyAddress(), MNCConsts.MULTICAST_ADDR, "grupa", MNCDatagram.TYPE.IAM_IN_GROUP, null));
-
-            Thread.sleep(15000);
-
-            System.out.println(controller2.getToken("grupa").toString());
-
+            if(args.length >= 3) {
+                MNCDevice device;
+                if (args[1].equals("C"))
+                    device = new MNCController(args[0], new MNCAddress(args[2], MNCAddress.TYPE.CONTROLLER), new MNCSystemLog(lang, args[0]));
+                else
+                    device = new MNCMonitor(args[0], new MNCAddress(args[2], MNCAddress.TYPE.MONITOR), new MNCSystemLog(lang, args[0]));
+                for (int i = 3; i < args.length; i++)
+                    device.addGroup(args[i]);
+            }
         } catch (java.io.IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
