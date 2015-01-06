c228ed3da5fb513877c996477fee553d1c66459a
przemek <przlada@gmail.com>
2015-01-06 01:34:05 +0100

    Sterowanie z lini polecen

--- src/pl/edu/pw/elka/tin/MNC/Main.java
+++ src/pl/edu/pw/elka/tin/MNC/Main.java
@@ -5,13 +5,15 @@ import pl.edu.pw.elka.tin.MNC.MNCController.MNCController;
 import pl.edu.pw.elka.tin.MNC.MNCController.MNCDevice;
 import pl.edu.pw.elka.tin.MNC.MNCController.MNCMonitor;
 
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        String command;
+        Scanner in = new Scanner(System.in);
         MNCDict.Langs lang = MNCDict.Langs.PL;
-        System.out.println("działa");
         try {
             if(args.length >= 3) {
                 MNCDevice device;
@@ -21,10 +23,18 @@ public class Main {
                     device = new MNCMonitor(args[0], new MNCAddress(args[2], MNCAddress.TYPE.MONITOR), new MNCSystemLog(lang, args[0]));
                 for (int i = 3; i < args.length; i++)
                     device.addGroup(args[i]);
+                while(true){
+                    command = in.nextLine();
+                    if(command.equals("token")){
+                        command = in.nextLine();
+                        System.out.println(((MNCController) device).getToken(command));
+                    }
+                }
             }
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
+        in.close();
 
 
     }
