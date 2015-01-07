3065cd22ae1d675541f1333f4e3bda6ee2c594fd
przemek <przlada@gmail.com>
2015-01-07 20:47:04 +0100

    Przekazanie Tokena

--- src/pl/edu/pw/elka/tin/MNC/MNCConstants/MNCConsts.java
+++ src/pl/edu/pw/elka/tin/MNC/MNCConstants/MNCConsts.java
@@ -11,7 +11,8 @@ import java.util.Random;
 public interface MNCConsts {
     public static final String LOCAL_LINE_SEPARATOR = System.getProperty("line.separator");
 
-    public static final MNCAddress MULTICAST_ADDR = new MNCAddress("ff02::1:1:1", MNCAddress.TYPE.MULTICAST_GROUP);
+    //public static final MNCAddress MULTICAST_ADDR = new MNCAddress("ff02::1:1:1", MNCAddress.TYPE.MULTICAST_GROUP);
+    public static final MNCAddress MULTICAST_ADDR = new MNCAddress("ff05::1:1:1", MNCAddress.TYPE.MULTICAST_GROUP);
     public static final int MCAST_PORT = 4446;
     public static final int UCAST_PORT = 4446;
 
