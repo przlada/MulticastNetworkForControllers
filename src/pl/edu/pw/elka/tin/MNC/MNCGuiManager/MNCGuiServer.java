84948c1172c8e1d681198c201c0678961a5bedae
przemek <przlada@gmail.com>
2015-01-11 11:44:26 +0100

    Program testujacy dzialanie multicastu

---
+++ src/pl/edu/pw/elka/tin/MNC/MNCGuiManager/MNCGuiServer.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.elka.tin.MNC.MNCGuiManager;
+
+/**
+ * Created by przemek on 10.01.15.
+ */
+public class MNCGuiServer implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
